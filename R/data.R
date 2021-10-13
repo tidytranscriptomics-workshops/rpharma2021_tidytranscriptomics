ccd421e4275094caa72c152fa931c291822c1b0c
stemangiola <mangiolastefano@gmail.com>
2021-10-14 09:30:36 +1100

    first commit

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' BRCA
+#'
+#' Subset of the TCGA BRCA data set
+#'
+#' @format  A tidy data frame.
+#' @usage data(BRCA)
+"BRCA"
+
+#' pbmc
+#'
+#' A SingleCellExperiment dataset of single cell RNA sequencing data
+#'
+#' @format  A SingleCellExperiment object.
+#' @usage data(pbmc)
+"pbmc"
+
+#' a subset version of pbmc in seurat format
+#'
+#' A Seurat dataset of single cell RNA sequencing data
+#'
+#' @format  A Seurat object.
+#' @usage data(pbmc_seurat)
+"pbmc_seurat"
+
+#' mito_info_all_datasets
+#'
+#' A SingleCellExperiment dataset of single cell RNA sequencing data. Used for one of the exercises
+#'
+#' @format  A Seurat object.
+#' @usage data(mito_info_all_datasets)
+"mito_info_all_datasets"
